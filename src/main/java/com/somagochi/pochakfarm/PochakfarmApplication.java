ac1648fd37914c0b6fece72be423ea18e2bb17dc
j-yong98 <chlwodyd9829@gmail.com>
2026-05-21 23:38:01 +0900

    project initial setting

---
+++ src/main/java/com/somagochi/pochakfarm/PochakfarmApplication.java
@@ -0,0 +1,13 @@
+package com.somagochi.pochakfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PochakfarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PochakfarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/somagochi/pochakfarm/PochakfarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somagochi.pochakfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PochakfarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
