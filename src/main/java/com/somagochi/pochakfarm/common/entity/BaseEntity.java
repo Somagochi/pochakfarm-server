4a84d5d9f563647d77868622a02597bca4f72ca4
j-yong98 <chlwodyd9829@gmail.com>
2026-06-23 12:14:33 +0900

    feat: BaseEntity 추가

---
+++ src/main/java/com/somagochi/pochakfarm/common/entity/BaseEntity.java
@@ -0,0 +1,35 @@
+package com.somagochi.pochakfarm.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private Instant createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at", nullable = false)
+  private Instant updatedAt;
+
+  @Column(name = "deleted_at")
+  private Instant deletedAt;
+
+  public boolean isDeleted() {
+    return deletedAt != null;
+  }
+
+  public void delete(Instant deletedAt) {
+    this.deletedAt = deletedAt;
+  }
+}
