274a1a3805cbb57e8f6ca74b9d371b44f4bf4df5
j-yong98 <chlwodyd9829@gmail.com>
2026-07-07 13:41:55 +0900

    fix: security config refresh 엔드포인트 허용

--- src/main/java/com/somagochi/pochakfarm/common/config/SecurityConfig.java
+++ src/main/java/com/somagochi/pochakfarm/common/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
   };
 
   private static final String[] PUBLIC_POST_ENDPOINTS = {
-    "/api/auth/login", "/api/characterizations", "/api/pre-registrations"
+    "/api/auth/login", "/api/characterizations", "/api/pre-registrations", "/api/auth/refresh"
   };
 
   private static final String[] PUBLIC_DELETE_ENDPOINTS = {"/api/pre-registrations"};
