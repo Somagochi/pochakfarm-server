a8e2e689b95a6b9bed64381747e6d42f7fdd4661
j-yong98 <chlwodyd9829@gmail.com>
2026-07-31 17:12:39 +0900

    fix: nickname max length

--- src/main/java/com/somagochi/pochakfarm/user/domain/User.java
+++ src/main/java/com/somagochi/pochakfarm/user/domain/User.java
@@ -30,7 +30,7 @@ import org.hibernate.annotations.SQLRestriction;
 @SQLRestriction("deleted_at is null")
 public class User extends BaseEntity {
 
-  private static final int MAX_NICKNAME_LENGTH = 20;
+  private static final int MAX_NICKNAME_LENGTH = 6;
   private static final int INITIAL_LEVEL = 1;
   private static final long INITIAL_COINS = 1000L;
 
