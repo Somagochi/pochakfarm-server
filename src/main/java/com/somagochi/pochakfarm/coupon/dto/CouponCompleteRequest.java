e6ecee0c4ccecfaf1a2cf8b87f550244edc5f975
j-yong98 <chlwodyd9829@gmail.com>
2026-08-07 18:19:14 +0900

    refactor: 쿠폰 완료 요청에서 미사용 animalImageKey 필드 제거

--- src/main/java/com/somagochi/pochakfarm/coupon/dto/CouponCompleteRequest.java
+++ src/main/java/com/somagochi/pochakfarm/coupon/dto/CouponCompleteRequest.java
@@ -3,6 +3,4 @@ package com.somagochi.pochakfarm.coupon.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record CouponCompleteRequest(
-    @Schema(description = "사전예약 쿠폰 코드", example = "POCHAK-A1B2C3D4") String couponCode,
-    @Schema(description = "클라이언트가 업로드한 누끼(동물) 이미지 키", example = "images/coupon-animal/1/uuid.png")
-        String animalImageKey) {}
+    @Schema(description = "사전예약 쿠폰 코드", example = "POCHAK-A1B2C3D4") String couponCode) {}
